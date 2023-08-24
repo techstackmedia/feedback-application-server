84324f3f25487f302f1c9ef697365f08cf10244b
Bello Osagie <osagienoah04@gmail.com>
2023-08-24 12:04:40 -0700

    prettier effect

--- routes/feedback.js
+++ routes/feedback.js
@@ -28,7 +28,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {      
-  
   try {
     const feedback = await Feedback.findById(req.params.id);
     if (!feedback) {
