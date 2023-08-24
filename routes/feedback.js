d25c9f4b7618e842d9e5871cce7cd70c155388f2
Bello Osagie <osagienoah04@gmail.com>
2023-08-24 14:32:48 -0700

    commit changes - no pagination

--- routes/feedback.js
+++ routes/feedback.js
@@ -13,7 +13,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {      
-  
   try {
     const feedback = await Feedback.findById(req.params.id);
     if (!feedback) {
